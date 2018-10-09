1efb2a594ea3d2fde81ca0a3fdf33329d4345482
Steffen Hayes <shayes238@e104-mb-07.mchenry.edu>
2018-10-09 15:20:55 -0500

    Initial Commit

---
+++ single_photo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  single_photo
+//
+//  Created by Steffen Hayes on 10/9/18.
+//  Copyright © 2018 Steffen Hayes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
